6eeb27c3dc02af890d1ab22350c64f5c0f2cee5e
jktham-sf <jonas.tham@gmx.ch>
2025-01-04 21:44:32 +0100

    fix calendar layout

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -2,25 +2,31 @@ import styles from "@/styles/calendar.module.css";
 import { Data, getLocalData } from "@/utils/dataUtils";
 import { dateToString, generateCalendar } from "@/utils/dateUtils";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const colorScheme = ["#00000000", "#ff0000", "#aaaa00", "#0000ff", "#aa00aa", "#00aa00"];
+const colorScheme = ["#00000000", "#005ae0", "#00b4dd", "#ffa10a", "#ff600a", "#f50062"];
 
 export default function Calendar() {
 	const year = new Date().getUTCFullYear();
-	const cal = generateCalendar(year);
+	const cal = generateCalendar(year-3, year);
 
 	const [data, setData] = useState<Data>();
 	const [date, setDate] = useState<string>("2001-01-01");
+
+	const activeDay = useRef<HTMLAnchorElement>(null);
 
 	useEffect(() => {
 		setData(getLocalData());
 		setDate(dateToString(new Date()));
 	}, []);
 
+	useEffect(() => {
+		if (activeDay.current) activeDay.current.scrollIntoView({block: "start"});
+	});
+
 	return (
 		<div className={styles.calendar}>
-			{Array.from(cal).map(([y, months]) => 
+			{Array.from(cal).reverse().map(([y, months]) => 
 				<div className={styles.year} key={`${y}`}>
 					<div className={styles.yearTitle}>
 						{y}
@@ -33,7 +39,7 @@ export default function Calendar() {
 								</div>
 								<div className={styles.days}>
 									{days.map((d) =>
-										<Link className={`${styles.day} ${date == `${y}-${m}-${d}` ? styles.active : ""}`} style={{backgroundColor: `${colorScheme[data?.get(`${y}-${m}-${d}`)?.rating || 0] || "#00000000"}`}} key={`${y}-${m}-${d}`} href={`/edit/${y}-${m}-${d}`}>
+										<Link className={`${styles.day} ${date == `${y}-${m}-${d}` ? styles.active : ""}`} style={{backgroundColor: `${colorScheme[data?.get(`${y}-${m}-${d}`)?.rating || 0] || "#00000000"}`}} key={`${y}-${m}-${d}`} href={`/edit/${y}-${m}-${d}`} ref={date == `${y}-${m}-${d}` ? activeDay : undefined}>
 											{d}
 										</Link>
 									)}
