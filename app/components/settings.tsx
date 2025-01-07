84c5e1022360bdda1b07b741d081fcd2cd8a928a
jktham-sf <jonas.tham@gmx.ch>
2025-01-07 22:47:27 +0100

    localstorage persistence

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -1,7 +1,18 @@
 import styles from "~/styles/settings.module.css";
 import { deleteLocalData, exportLocalData, importLocalData } from "~/lib/dataUtils";
+import { useEffect, useState } from "react";
 
 export default function Settings() {
+	const [persistent, setPersistent] = useState<boolean>(false);
+
+	const requestPersistence = () => {
+		navigator.storage.persist().then((p) => setPersistent(p));
+	};
+
+	useEffect(() => {
+		navigator.storage.persisted().then((p) => setPersistent(p));
+	}, []);
+
 	return (
 		<div className={styles.settings}>
 			<div className={"titleBar"}>
@@ -14,6 +25,11 @@ export default function Settings() {
 				<button className={"btn"} onClick={() => importLocalData()}>Import<span className="material-symbols-outlined">upload</span></button>
 				<button className={"btn"} onClick={() => exportLocalData()}>Export<span className="material-symbols-outlined">download</span></button>
 				<button className={"btn"} onClick={() => deleteLocalData()}>Delete<span className="material-symbols-outlined">delete</span></button>
+			</div>
+			<div className={styles.section}>
+				<div className={styles.sectionTitle}>Storage</div>
+				<div>{persistent ? "Persistent" : "Not persistent"}</div>
+				<button className={"btn"} onClick={() => requestPersistence()}>Request</button>
 			</div>
 		</div>
 	);
